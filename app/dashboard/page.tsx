2abc1a85a3c7fcd2feb94730c2d21f7507181ef7
Sagar Tamang <sagar.bdr0000@gmail.com>
2025-12-25 20:23:33 +0530

    feat/chore: impl metadata @ dashboard routes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import { ComponentExample } from "@/components/component-example";
 
+export const metadata: Metadata = {
+  title: "My Duels | Dashboard", 
+};
+
 export default function Page() {
-return <ComponentExample />;
+  return <ComponentExample />;
 }
